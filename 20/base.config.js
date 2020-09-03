d4955e95a8bddf8ac59840fd30873353f264ac49
aisuhua <1079087531@qq.com>
2020-09-03 21:52:45 +0800

    finish

--- 20/base.config.js
+++ 20/base.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/js/index.js',
@@ -121,53 +119,10 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/index.html',
             filename: 'index.html',
-            minify: {
-                collapseWhitespace: false, // 删除空格
-                removeComments: false // 删除注释
-            }
         }),
         // 打包 CSS 文件
         new MiniCssExtractPlugin({
             filename: 'css/[name]-[hash].css'
-        }),
-        // 清理打包文件
-        new CleanWebpackPlugin(),
-        // 压缩 CSS 文件
-        // new OptimizeCssAssetsWebpackPlugin()
-    ],
-
-    devServer: {
-        contentBase: path.resolve(__dirname, './dist'),
-        port: 3000,
-        stats: 'minimal',
-        // https://github.com/iuap-design/blog/issues/260
-        proxy: {
-            '/api': {
-                target: 'https://cdn.ycku.com',
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/api': ''
-                }
-            }
-        }
-    },
-
-    // 监听打包文件，变化后自动重新打包
-    watch: false,
-
-    // 监听选项
-    watchOptions: {
-        // 不监听的目录
-        ignored: /node_modules/,
-        // 更新频率，有变化后 500 毫秒再编译
-        aggregateTimeout: 500,
-        // 轮询间隔时间，默认1秒，询问系统文件是否变化
-        poll: 1000
-    },
-
-    // 当 js 脚本发生报错时，能对应到源文件所在位置
-    // 开发环境用 eval-source-map，生产用 source-map
-    devtool: "eval-source-map",
-
-    mode: "development"
+        })
+    ]
 };
